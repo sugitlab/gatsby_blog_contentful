c494ed69aa1d71d34d20099778ef6d4b79d0b0a7
sugitlab <sgmt.snj@gmail.com>
2021-03-17 08:29:13 +0900

    zenn_gatsby chapter18-contentintocomponent

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,23 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Homepage",
+    description: "Sugit Laboratory",
+    author: "sugit"
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_API_KEY
+      }
+    }
+  ],
 }
